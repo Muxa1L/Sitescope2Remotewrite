75631fdcd464869a545c56737f4276c514fda37a
Mikhail Davydenko <mimih1990@gmail.com>
2021-03-29 18:15:29 +0300

    now

--- Processing/MonitorProcessor.cs
+++ Processing/MonitorProcessor.cs
@@ -102,7 +102,7 @@ namespace Sitescope2RemoteWrite.Processing
             //monitor.timestamp = DateTime.UtcNow.ToUnixTimeStamp() * 1000;
             var timeSeries = RegexProcess.ProcessCounters(baseTS, monitor);
             ///TODO ACHTUNG! ABSOLUTE PIZTETS!!!
-            timeSeriesQueue.EnqueueList(timeSeries);
+            //timeSeriesQueue.EnqueueList(timeSeries);
         }
     }
 }
